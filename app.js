8c6ba1d8e28fb0a46fc190385b134d09984bac09
Jopiol <you@example.com>
2023-12-06 22:10:47 +0400

    more functional (darkmode & lbs choice) fixed some bugs

--- app.js
+++ app.js
@@ -1,42 +1,140 @@
-//TODO:list of possible reps; ibs variant; darkmode;
+//TODO:list of possible reps; darkmode;
 
 //variables of elements
 const weightInput = document.getElementById("weight");
 const repsInput = document.getElementById("reps");
 const calcButton = document.getElementById("calc");
 const para = document.getElementById("para");
-const kg = document.getElementById("index");
+const index = document.getElementById("index");
 const count = document.getElementById("count");
+const checkBox = document.getElementById("var1");
+const checkBox2 = document.getElementById("var2");
+const container = document.getElementById("container");
+const darkModeButton = document.getElementById("darkmode__button");
+//adding dark mode class on elements after user clicked on darkmode button
+darkModeButton.textContent = "Dark Mode";
+darkModeButton.value = "dark"
 
+darkModeButton.addEventListener("click", () => {
+  if (darkModeButton.value === "dark") {
+    // Switch to dark mode
+    container.classList.add("dark");
+    weightInput.classList.add("dark");
+    repsInput.classList.add("dark");
+    calcButton.classList.add("dark");
+    para.classList.add("dark");
+    index.classList.add("dark");
+    count.classList.add("dark");
+    darkModeButton.classList.add("dark");
+    darkModeButton.textContent = "Light Mode";
+    darkModeButton.value = "light";
+  } else if (darkModeButton.value === "light") {
+    // Switch to light mode
+    container.classList.remove("dark");
+    weightInput.classList.remove("dark");
+    repsInput.classList.remove("dark");
+    calcButton.classList.remove("dark");
+    para.classList.remove("dark");
+    index.classList.remove("dark");
+    count.classList.remove("dark");
+    darkModeButton.classList.remove("dark");
+    darkModeButton.textContent = "Dark Mode";
+    darkModeButton.value = "dark";
+  }
+});
+//change input field with arrow key
+repsInput.addEventListener("keydown", (e) =>{
+  if (e.key === "ArrowRight") {
+    e.preventDefault();
+    weightInput.focus();
+  }
+});
+weightInput.addEventListener("keydown", (e) =>{
+  if (e.key === "ArrowLeft") {
+    e.preventDefault();
+    repsInput.focus();
+  }
+});
+//Event listener for checkboxes
+checkBox.addEventListener("change",()=> {
+  if (checkBox.checked) {
+    checkBox2.checked = false;
+    index.textContent = "kg";
+    para.textContent = "";
+    count.textContent = "";
+  }
+});
+checkBox2.addEventListener("change",() =>{
+  if (checkBox2.checked) {
+    checkBox.checked = false;
+    index.textContent = "lbs";
+    para.textContent = "";
+    count.textContent = "";
+  }
+});
+//Default startup (kg)
+document.addEventListener("DOMContentLoaded", () =>{
+  checkBox.checked = !checkBox2.checked;
+  index.textContent = "kg";
+});
 //main event listener
 calcButton.addEventListener('click',()=>{
-  //checking if value of input valley isn't empty or negative
-   if(weightInput.value !== "" && repsInput.value !== "" && weightInput.value > 0 && repsInput.value > 0){
-        //main logic of calculator
-        const answer = (repsInput.value / 30 + 1) * weightInput.value;
-        para.textContent = Math.round(answer);
-        kg.textContent = "Kg";
-        count.textContent = "x1"
-        weightInput.value = "";
-        repsInput.value = "";
-        weightInput.classList.remove("active");
-        repsInput.classList.remove("active");
-
-        //adding active status if numbers from input are empty or negative
-   }else {
-     if (weightInput.value === "" || weightInput.value < 0) {
-         weightInput.classList.add("active");
-     } else {
-         weightInput.classList.remove("active");
-     }
-     if (repsInput.value === "" || repsInput.value < 0) {
-         repsInput.classList.add("active");
-     } else {
-         repsInput.classList.remove("active");
-     }
+  //kg variant 
+   if(checkBox.checked){
+    //checking if value of input valley isn't empty, negative or integer
+    if(weightInput.value !== "" && repsInput.value !== "" && weightInput.value > 0 && repsInput.value > 0 && repsInput.value % 1 === 0){
+      //main logic of calculator (kg)
+      //calculation formula
+      const answer = (repsInput.value / 30 + 1) * weightInput.value;
+      para.textContent = Math.round(answer);
+      count.textContent = "x1"
+      weightInput.value = "";
+      repsInput.value = "";
+      weightInput.classList.remove("active");
+      repsInput.classList.remove("active");
+      //adding active status if numbers from input are empty, negative or integer
+ }else {
+   if (weightInput.value === "" || weightInput.value < 0) {
+       weightInput.classList.add("active");
+   } else {
+       weightInput.classList.remove("active");
+   }
+   if (repsInput.value === "" || repsInput.value < 0 || repsInput.value % 1 !== 0) {
+       repsInput.classList.add("active");
+   } else {
+       repsInput.classList.remove("active");
+   }
+}   //lbs variant
+   }else{
+    checkBox2.checked = !checkBox.checked;
+      //checking if value of input valley isn't empty, negative or integer
+    if(weightInput.value !== "" && repsInput.value !== "" && weightInput.value > 0 && repsInput.value > 0 && repsInput.value % 1 === 0){
+      //main logic of calculator (lbs)
+      const lbsReps = parseInt(repsInput.value);
+      const lbsWeight = parseInt(weightInput.value);
+      // Calculate bench press max in lbs
+      const answer = (lbsReps / 30 + 1) * lbsWeight;
+      para.textContent = Math.round(answer);
+      count.textContent = "x1"
+      weightInput.value = "";
+      repsInput.value = "";
+      weightInput.classList.remove("active");
+      repsInput.classList.remove("active");
+      //adding active status if numbers from input are empty, negative or integer
+ }else {
+   if (weightInput.value === "" || weightInput.value < 0) {
+       weightInput.classList.add("active");
+   } else {
+       weightInput.classList.remove("active");
+   }
+   if (repsInput.value === "" || repsInput.value < 0 || repsInput.value % 1 !== 0) {
+       repsInput.classList.add("active");
+   } else {
+       repsInput.classList.remove("active");
+   }
 }
+   }
 });
-
 //removing active status if all statements are true
 weightInput.addEventListener('input', () => {
   weightInput.classList.remove("active");
@@ -44,16 +142,15 @@ weightInput.addEventListener('input', () => {
 repsInput.addEventListener('input', () => {
   repsInput.classList.remove("active");
 });
-
-//input valley restriction for only 3 number
+//input valley restriction (up to 4)
 weightInput.addEventListener('input', () => {
-  if (weightInput.value.length > 3) {
-      weightInput.value = weightInput.value.slice(0, 3);
+  if (weightInput.value.length > 4) {
+      weightInput.value = weightInput.value.slice(0, 4);
   }
 });
 repsInput.addEventListener('input', () => {
-  if (repsInput.value.length > 3) {
-      repsInput.value = repsInput.value.slice(0, 3);
+  if (repsInput.value.length > 4) {
+      repsInput.value = repsInput.value.slice(0, 4);
   }
 });
 
@@ -75,5 +172,5 @@ i/ *   O   O   * \i
 /=================\
        |___|
 
-last edit: 04.12.2023
+last edit: 06.12.2023
 */
