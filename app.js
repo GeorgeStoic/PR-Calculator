fe453189b697f06ef2ddfc6e7a9da839d43f5bc9
Jopiol <you@example.com>
2023-12-07 11:59:46 +0400

    fixed bugs & added some features

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//TODO:list of possible reps; darkmode;
+//TODO:list of possible reps;
 
 //variables of elements
 const weightInput = document.getElementById("weight");
@@ -11,10 +11,11 @@ const checkBox = document.getElementById("var1");
 const checkBox2 = document.getElementById("var2");
 const container = document.getElementById("container");
 const darkModeButton = document.getElementById("darkmode__button");
+const switcher = document.getElementById("switch");
+const contentDiv = document.getElementById("content");
 //adding dark mode class on elements after user clicked on darkmode button
 darkModeButton.textContent = "Dark Mode";
 darkModeButton.value = "dark"
-
 darkModeButton.addEventListener("click", () => {
   if (darkModeButton.value === "dark") {
     // Switch to dark mode
@@ -26,6 +27,7 @@ darkModeButton.addEventListener("click", () => {
     index.classList.add("dark");
     count.classList.add("dark");
     darkModeButton.classList.add("dark");
+    switcher.classList.add("dark");
     darkModeButton.textContent = "Light Mode";
     darkModeButton.value = "light";
   } else if (darkModeButton.value === "light") {
@@ -38,8 +40,20 @@ darkModeButton.addEventListener("click", () => {
     index.classList.remove("dark");
     count.classList.remove("dark");
     darkModeButton.classList.remove("dark");
+    switcher.classList.remove("dark");
     darkModeButton.textContent = "Dark Mode";
     darkModeButton.value = "dark";
+  }
+});
+//switcher
+contentDiv.value = "row";
+switcher.addEventListener("click", ()=>{
+  if(contentDiv.value == "row"){
+    contentDiv.style = "flex-direction: row-reverse;"
+    contentDiv.value = "row-reversed"
+  }else{
+    contentDiv.style = "flex-direction: row;"
+    contentDiv.value = "row"
   }
 });
 //change input field with arrow key
@@ -79,20 +93,31 @@ document.addEventListener("DOMContentLoaded", () =>{
 });
 //main event listener
 calcButton.addEventListener('click',()=>{
+  //checking if user entered number from 1 to 10
+  if(repsInput.value <= 10 && repsInput.value >= 1){
   //kg variant 
    if(checkBox.checked){
     //checking if value of input valley isn't empty, negative or integer
     if(weightInput.value !== "" && repsInput.value !== "" && weightInput.value > 0 && repsInput.value > 0 && repsInput.value % 1 === 0){
-      //main logic of calculator (kg)
+      if(repsInput.value <= 1){
+      para.textContent = weightInput.value;
+      count.textContent = "x1";
+      weightInput.value = "";
+      repsInput.value = "";
+      weightInput.classList.remove("active");
+      repsInput.classList.remove("active");
+      }else{
+        //main logic of calculator (kg)
       //calculation formula
       const answer = (repsInput.value / 30 + 1) * weightInput.value;
       para.textContent = Math.round(answer);
-      count.textContent = "x1"
+      count.textContent = "x1";
       weightInput.value = "";
       repsInput.value = "";
       weightInput.classList.remove("active");
       repsInput.classList.remove("active");
       //adding active status if numbers from input are empty, negative or integer
+      }
  }else {
    if (weightInput.value === "" || weightInput.value < 0) {
        weightInput.classList.add("active");
@@ -109,17 +134,26 @@ calcButton.addEventListener('click',()=>{
     checkBox2.checked = !checkBox.checked;
       //checking if value of input valley isn't empty, negative or integer
     if(weightInput.value !== "" && repsInput.value !== "" && weightInput.value > 0 && repsInput.value > 0 && repsInput.value % 1 === 0){
-      //main logic of calculator (lbs)
+      if(repsInput.value <= 1){
+        para.textContent = weightInput.value;
+        count.textContent = "x1";
+        weightInput.value = "";
+        repsInput.value = "";
+        weightInput.classList.remove("active");
+        repsInput.classList.remove("active");
+      }else{
+        //main logic of calculator (lbs)
       const lbsReps = parseInt(repsInput.value);
       const lbsWeight = parseInt(weightInput.value);
       // Calculate bench press max in lbs
       const answer = (lbsReps / 30 + 1) * lbsWeight;
       para.textContent = Math.round(answer);
-      count.textContent = "x1"
+      count.textContent = "x1";
       weightInput.value = "";
       repsInput.value = "";
       weightInput.classList.remove("active");
       repsInput.classList.remove("active");
+      }
       //adding active status if numbers from input are empty, negative or integer
  }else {
    if (weightInput.value === "" || weightInput.value < 0) {
@@ -134,6 +168,9 @@ calcButton.addEventListener('click',()=>{
    }
 }
    }
+  }else{
+    alert("Please enter rep amount from 1 to 10");
+  }
 });
 //removing active status if all statements are true
 weightInput.addEventListener('input', () => {
@@ -172,5 +209,5 @@ i/ *   O   O   * \i
 /=================\
        |___|
 
-last edit: 06.12.2023
+last edit: 07.12.2023
 */
